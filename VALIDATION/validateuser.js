9017609432b8915613698f377e5e01718b688d60
BetiYEG <120360351+BetiYEG@users.noreply.github.com>
2023-02-12 22:51:25 +0300

    Add files via upload

---
+++ VALIDATION/validateuser.js
@@ -0,0 +1,18 @@
+
+async function validateUser(username, password){
+    const user = await fetch("localhost:3000/findOne(username)")
+    method:'Get'
+    headers: {'Content-Type' ; 'application/json'}
+    
+    Body:JSON.stringify({ 
+        id: id,
+        username: username,
+        password:password,
+    });
+    if (user && user.password === password) {
+        const {password, ...result} = user
+        return result
+    }
+    return null
+
+} 
